aa7d6ce03402e86fbe848a16cebc5c9ac1ac83b6
Jonathan Schang <schang@Jonathans-MacBook-Pro.local>
2015-02-28 13:17:48 -0800

    Initial Commit

---
+++ MCS7715/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MCS7715
+//
+//  Created by Jonathan Schang on 2/28/15.
+//  Copyright (c) 2015 Jon Schang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
